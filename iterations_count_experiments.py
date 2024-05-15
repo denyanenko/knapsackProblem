c9fd921041cb4e2ab997adf47a98169cd849c861
denyanenko <108097508+denyanenko@users.noreply.github.com>
2024-05-15 21:19:16 +0300

    solution to file

--- iterations_count_experiments.py
+++ iterations_count_experiments.py
@@ -2,7 +2,7 @@ from application.Application import experiments_input
 from probabilistic_algorithm.ProbabilisticAlgorithm import ProbabilisticAlgorithm
 
 
-def test_population_size():
+def run_test():
     count_experiments = 1000
     N = [10, 25, 50, 100, 250, 500, 750, 1000, 1500, 2000, 3000, 5000, 7000, 10000]
     n = 50
@@ -18,7 +18,7 @@ def test_population_size():
 
 
 def main():
-    test_population_size()
+    run_test()
 
 
 if __name__ == "__main__":
